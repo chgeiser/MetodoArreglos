30458f92727de1036cf3373ae16ebd50b755ab72
Christopher Geiser <christopher.geiser@gmail.com>
2025-10-27 23:49:23 -0300

    mejoras

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,45 @@
-const personas = [
-  /*{ nombre: "Christopher", apellido: "Geiser" },
-  { nombre: "Daniela", apellido: "Pincheira" },
-  { nombre: "David", apellido: "geiser Pincheira" },*/
-];
-let idActual= 1;
-/*console.log("primera vez: " + personas);
 
-personas.push({nombre:"Carmen", apellido:"Gonzalez"});
-personas.unshift({nombre:"Enrique", apellido:"Geiser"});
-console.log("Segunda Vez " + personas)*/
+
+const tareas = [];
+let idActual= 1;
+
+
+const dibujarTarea = function () {
+  const ulListaTareas = document.querySelector("#listaTarea");
+  let html = "";
+  for (const elemento of tareas) {
+    html += `<li><button onClick="eliminarTarea(${tareas.id});" type="button">Eliminar</button>${elemento.descripcion}</li>`;
+  }
+
+  ulListaTareas.innerHTML = html;
+};
+
+const agregarTarea = function () {
+  const inputNuevaTarea = document.querySelector("#nuevaTarea");
+  const objNuevaTarea = {
+    id: idActual,
+    descripcion: inputNuevaTarea.value,
+    realizado: false,
+  };
+  idActual++;
+
+  tareas.push(objNuevaTarea);
+  inputNuevaTarea.value="";
+  dibujarTarea();
+};
+
+const eliminarTarea = function(eliminarTareaId){
+    const index = tareas.findIndex((tarea) => tarea.id === eliminarTareaId);
+    tareas.splice(index,1);
+    console.log(tareas)
+    dibujarTarea();
+}
+/*
+const personas = [
+  { nombre: "Christopher", apellido: "Geiser" },
+  { nombre: "Daniela", apellido: "Pincheira" },
+  { nombre: "David", apellido: "geiser Pincheira" },
+];
 
 const dibujarLista = function () {
   const ulListaInvitado = document.querySelector("#listaInvitado");
@@ -19,13 +50,12 @@ const dibujarLista = function () {
 
   ulListaInvitado.innerHTML = html;
 };
-
 const agregarPersona = function () {
   const inputPersona = document.querySelector("#nuevoNombre");
   const objNuevaPersona = {
     id: idActual,
     nombre: inputPersona.value,
-    apellido: "",
+    apellido: '',
   };
   idActual++;
 
@@ -33,15 +63,19 @@ const agregarPersona = function () {
   inputPersona.value="";
   dibujarLista();
 };
-
 const eliminarPersona = function(personaEliminar){
     const index = personas.findIndex((personas) => personas.id === personaEliminar);
     personas.splice(index,1);
     console.log(personas)
     dibujarLista();
-}
+}*/
 
-dibujarLista();
+
+
+
+
+//dibujarLista();
+dibujarTarea();
 
 //eliminar
 /*const index = personas.findIndex((personas) => personas.nombre ==="Christopher");
